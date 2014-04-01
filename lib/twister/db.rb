f2f263b62f465d4aa8dd1034c7a1887b1b3b1619
Alpha Chen <alpha.chen@gmail.com>
2014-03-31 18:07:10 -0700

    Use timestamps

--- lib/twister/db.rb
+++ lib/twister/db.rb
@@ -1,9 +1,11 @@
 require 'logger'
 
 require 'sequel'
+Sequel::Model.plugin :timestamps
 
 module Twister
   DB = Sequel.connect(ENV.fetch('DATABASE_URL'))
   DB.extension :pg_array
+
   # DB.loggers << Logger.new(STDOUT)
 end
