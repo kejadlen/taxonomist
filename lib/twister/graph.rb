f7fe7f5784246fb9ac74f9bdd4a0f63e23e147a2
Alpha Chen <alpha.chen@gmail.com>
2014-05-28 17:01:16 -0700

    Add Brandes betweenness centrality for vertices

--- lib/twister/graph.rb
+++ lib/twister/graph.rb
@@ -1,25 +1,76 @@
 require 'set'
 
 module Twister
-  class Graph
-    attr_reader :nodes, :links
+  Node = Struct.new(:id, :neighbors)
 
-    def initialize(friend)
-      @nodes = friend.friend_ids.reject {|id| id == friend.twitter_id }
-      @links = friend.friends.reject {|f| f == friend }
-                             .each.with_object(Set.new) do |friend, links|
-        friend.friend_ids.reject {|id| id == friend.twitter_id }
-                         .each do |friend_id|
-          links << [friend.twitter_id, friend_id]
-          links << [friend_id, friend.twitter_id]
-        end
+  class Graph
+    # attr_reader :nodes, :links
+
+    # def initialize(friend)
+    #   @nodes = friend.friend_ids.reject {|id| id == friend.twitter_id }
+    #   @links = friend.friends.reject {|f| f == friend }
+    #                          .each.with_object(Set.new) do |friend, links|
+    #     friend.friend_ids.reject {|id| id == friend.twitter_id }
+    #                      .each do |friend_id|
+    #       links << [friend.twitter_id, friend_id]
+    #       links << [friend_id, friend.twitter_id]
+    #     end
+    #   end
+    # end
+
+    # def betweenness
+    #   fw = FloydWarshall.new(nodes,
+    #                          links.each.with_object({}) {|l,h| h[l] = 1 })
+    #   fw.calculate!
+    # end
+  end
+
+  class Brandes
+    def self.from_friend(friend)
+      friends = friend.friends.reject {|f| f == friend }
+      friend_ids = friends.map(&:twitter_id)
+      neighbors = friends.each.with_object({}) do |friend, neighbors|
+        neighbors[friend.twitter_id] = friend.friend_ids.select {|id| friend_ids.include?(id) }
       end
+      new(neighbors)
     end
 
-    def betweenness
-      fw = FloydWarshall.new(nodes,
-                             links.each.with_object({}) {|l,h| h[l] = 1 })
-      fw.calculate!
+    attr_reader :neighbors
+
+    def initialize(neighbors)
+      @neighbors = neighbors
+    end
+
+    def betweenness_centrality
+      c_b = Hash.new(0)
+      neighbors.keys.each do |s|
+        stack = []
+        p = Hash.new {|h,k| h[k] = [] }
+        𝜎 = Hash.new(0); 𝜎[s] = 1
+        d = Hash.new; d[s] = 0
+        q = [s]
+        until q.empty?
+          v = q.shift
+          stack << v
+          neighbors[v].each do |w|
+            unless d.has_key?(w)
+              q << w
+              d[w] = d[v] + 1
+            end
+            if d[w] == d[v] + 1
+              𝜎[w] = 𝜎[w] + 𝜎[v]
+              p[w] << v
+            end
+          end
+        end
+        𝛿 = Hash.new(0)
+        until stack.empty?
+          w = stack.pop
+          p[w].each {|v| 𝛿[v] = 𝛿[v] + (𝜎[v]/𝜎[w])*(1 + 𝛿[w]) }
+          c_b[w] = c_b[w] + 𝛿[w] unless w == s
+        end
+      end
+      c_b
     end
   end
 
