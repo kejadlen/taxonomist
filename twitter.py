e401b4f9de885893ccddc95e1a7c466b445fd77a
Alpha Chen <alpha.chen@gmail.com>
2014-09-05 16:32:17 -0700

    Fix bug in Twitter adapter

--- twitter.py
+++ twitter.py
@@ -16,7 +16,7 @@ class Twitter:
                                 os.environ['API_SECRET'],
                                 callback_uri='http://127.0.0.1:5000/callback')
 
-        return twitter.fetch_request_token(url_for('/oauth/request_token'))
+        return twitter.fetch_request_token(cls.url_for('/oauth/request_token'))
 
     @classmethod
     def access_token(cls, oauth_token, oauth_token_secret, oauth_verifier):
@@ -26,7 +26,7 @@ class Twitter:
                                 resource_owner_secret=oauth_token_secret,
                                 verifier=oauth_verifier)
 
-        return twitter.fetch_access_token(url_for('/oauth/access_token'))
+        return twitter.fetch_access_token(cls.url_for('/oauth/access_token'))
 
     def __init__(self, oauth_token, oauth_token_secret):
         self.oauth = OAuth1Session(os.environ['API_KEY'],
@@ -36,12 +36,12 @@ class Twitter:
 
     def friends_ids(self, user_id):
         payload = {'user_id': user_id}
-        response = self.oauth.get(url_for('/1.1/friends/ids.json'),
+        response = self.oauth.get(self.url_for('/1.1/friends/ids.json'),
                                   params=payload)
         return (response.json().get('ids'), response)
 
     def users_lookup(self, user_ids):
         payload = {'user_id': ','.join([str(id) for id in user_ids])}
-        response = self.oauth.post(url_for('/1.1/users/lookup.json'),
+        response = self.oauth.post(self.url_for('/1.1/users/lookup.json'),
                                    data=payload)
         return (response.json(), response)
