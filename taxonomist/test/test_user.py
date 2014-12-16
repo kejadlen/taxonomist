0230ab47aaa5fc5005ab59a0428fdb481587c517
Alpha Chen <alpha.chen@gmail.com>
2014-12-16 11:26:57 -0800

    PEP8

--- taxonomist/test/test_user.py
+++ taxonomist/test/test_user.py
@@ -3,6 +3,7 @@ from taxonomist.test import TestCase
 from taxonomist.twitter import Session
 from taxonomist.twitter.user import User
 
+
 class TestUser(TestCase):
     def test_test_db(self):
         """ Verify that the test db is working as expected."""
