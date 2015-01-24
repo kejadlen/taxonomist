4aa873167cb02499e46df7ad9fb36488437781d3
Alpha Chen <alpha.chen@gmail.com>
2015-01-23 21:12:03 -0800

    Initial commit for a web frontend
    Use Flask and HTML5 Boilerplate. Twitter sign-in should also be working.

---
+++ taxonomist/web/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask, g, render_template, session
+import os
+
+from .. import db
+from ..models.user import User
+from ..twitter import Client
+
+app = Flask(__name__, static_url_path='')
+app.secret_key = os.environ["FLASK_SECRET"]
+
+
+@app.before_request
+def before_request():
+    db.init()
+
+    g.user = None
+    if 'user_id' in session:
+        g.user = User.query.get(session['user_id'])
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db.session.remove()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
