6c5cceea4d4d06c8ebebca232bc24d1ea3a4f2ef
Alpha Chen <alpha.chen@gmail.com>
2015-01-29 22:12:53 -0800

    Use scales to automatically orient viz

--- taxonomist/web/static/js/main.js
+++ taxonomist/web/static/js/main.js
@@ -1,29 +1,37 @@
 var width = parseInt(d3.select('#friend_graph').style('width'), 10),
     height = 400,
-    scaleFactor = 1,
-    translation = [0,0],
     link,
     node;
 
+var x = d3.scale.linear()
+    .domain([0,width])
+    .range([0,width]);
+var y = d3.scale.linear()
+    .domain([0,height])
+    .range([0,height]);
+
 var zoomer = d3.behavior.zoom()
     .scaleExtent([0.1,10])
-    .on("zoom", zoom);
+    .on("zoom", zoom)
+    .x(x)
+    .y(y);
 
 function zoom() {
-    console.log("zoom", d3.event.translate, d3.event.scale);
-    scaleFactor = d3.event.scale;
-    translation = d3.event.translate;
-    tick(); //update positions
+    // console.log("zoom", d3.event.translate, d3.event.scale);
+    tick();
 }
 
 function tick() {
-    link.attr("x1", function(d) { return translation[0] + scaleFactor*d.source.x; })
-        .attr("y1", function(d) { return translation[1] + scaleFactor*d.source.y; })
-        .attr("x2", function(d) { return translation[0] + scaleFactor*d.target.x; })
-        .attr("y2", function(d) { return translation[1] + scaleFactor*d.target.y; });
+    link.attr("x1", function(d) { return x(d.source.x); })
+        .attr("y1", function(d) { return y(d.source.y); })
+        .attr("x2", function(d) { return x(d.target.x); })
+        .attr("y2", function(d) { return y(d.target.y); });
 
-    node.attr("cx", function(d) { return translation[0] + scaleFactor*d.x; })
-        .attr("cy", function(d) { return translation[1] + scaleFactor*d.y; });
+    node.attr("cx", function(d) { return x(d.x); })
+        .attr("cy", function(d) { return y(d.y); });
+
+    x.domain(d3.extent(node.data(), function(d) { return d.x; }))
+    y.domain(d3.extent(node.data(), function(d) { return d.y; }))
 }
 
 var force = d3.layout.force()
@@ -35,8 +43,8 @@ var force = d3.layout.force()
 
 var svg = d3.select("#friend_graph").append("svg")
     .attr("width", width)
-    .attr("height", height)
-    .call(zoomer);
+    .attr("height", height);
+    // .call(zoomer);
 
 d3.json("friend_graph.json", function(error, graph) {
   force
