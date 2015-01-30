abaca87faf4872ce634e28a3ece2f8f1ede85b6d
Alpha Chen <alpha.chen@gmail.com>
2015-01-29 21:19:24 -0800

    Tweak friend graph settings

--- taxonomist/web/static/js/main.js
+++ taxonomist/web/static/js/main.js
@@ -1,9 +1,11 @@
 var width = parseInt(d3.select('#friend_graph').style('width'), 10),
-    height = 500;
+    height = 400;
 
 var force = d3.layout.force()
-    .charge(-120)
-    .linkDistance(30)
+    .linkDistance(50)
+    .friction(0.8)
+    .charge(-100)
+    .gravity(0.2)
     .size([width, height]);
 
 var svg = d3.select("#friend_graph").append("svg")
@@ -14,26 +16,18 @@ d3.json("friend_graph.json", function(error, graph) {
   force
       .nodes(graph.nodes)
       .links(graph.links)
-      .linkDistance(50)
-      .friction(0.8)
-      .charge(-100)
-      .gravity(0.2)
       .start();
 
   var link = svg.selectAll(".link")
       .data(graph.links)
     .enter().append("line")
-      .attr("class", "link")
-      // .style("stroke-width", function(d) { return Math.sqrt(d.value); })
-      // .style("visibility", "hidden");
+      .attr("class", "link");
 
   var node = svg.selectAll(".node")
       .data(graph.nodes)
     .enter().append("circle")
       .attr("class", "node")
       .attr("r", 2);
-      // .style("fill", function(d) { return color(d.group); })
-      // .call(force.drag);
 
   node.append("title")
       .text(function(d) { return d.sceen_name; });
