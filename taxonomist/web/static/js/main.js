a4b76d7a2e2d17b16568d7387e0547eeca4a235e
Alpha Chen <alpha.chen@gmail.com>
2015-01-29 21:54:23 -0800

    Panning and semantic zooming

--- taxonomist/web/static/js/main.js
+++ taxonomist/web/static/js/main.js
@@ -1,16 +1,42 @@
 var width = parseInt(d3.select('#friend_graph').style('width'), 10),
-    height = 400;
+    height = 400,
+    scaleFactor = 1,
+    translation = [0,0],
+    link,
+    node;
+
+var zoomer = d3.behavior.zoom()
+    .scaleExtent([0.1,10])
+    .on("zoom", zoom);
+
+function zoom() {
+    console.log("zoom", d3.event.translate, d3.event.scale);
+    scaleFactor = d3.event.scale;
+    translation = d3.event.translate;
+    tick(); //update positions
+}
+
+function tick() {
+    link.attr("x1", function(d) { return translation[0] + scaleFactor*d.source.x; })
+        .attr("y1", function(d) { return translation[1] + scaleFactor*d.source.y; })
+        .attr("x2", function(d) { return translation[0] + scaleFactor*d.target.x; })
+        .attr("y2", function(d) { return translation[1] + scaleFactor*d.target.y; });
+
+    node.attr("cx", function(d) { return translation[0] + scaleFactor*d.x; })
+        .attr("cy", function(d) { return translation[1] + scaleFactor*d.y; });
+}
 
 var force = d3.layout.force()
-    .linkDistance(50)
+    .linkDistance(25)
     .friction(0.8)
-    .charge(-100)
+    .charge(-75)
     .gravity(0.2)
     .size([width, height]);
 
 var svg = d3.select("#friend_graph").append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .call(zoomer);
 
 d3.json("friend_graph.json", function(error, graph) {
   force
@@ -18,12 +44,12 @@ d3.json("friend_graph.json", function(error, graph) {
       .links(graph.links)
       .start();
 
-  var link = svg.selectAll(".link")
+  link = svg.selectAll(".link")
       .data(graph.links)
     .enter().append("line")
       .attr("class", "link");
 
-  var node = svg.selectAll(".node")
+  node = svg.selectAll(".node")
       .data(graph.nodes)
     .enter().append("circle")
       .attr("class", "node")
@@ -32,13 +58,5 @@ d3.json("friend_graph.json", function(error, graph) {
   node.append("title")
       .text(function(d) { return d.sceen_name; });
 
-  force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-    node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-  });
+  force.on("tick", tick)
 });
