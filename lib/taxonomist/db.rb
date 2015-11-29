6afcc35e7bab7a2dd73a2d30b28d4150c207b81c
Alpha Chen <alpha.chen@gmail.com>
2015-11-28 17:22:26 -0800

    User#graph

--- lib/taxonomist/db.rb
+++ lib/taxonomist/db.rb
@@ -7,7 +7,14 @@ module Taxonomist
   DB.extension :pg_array, :pg_json
 
   module Models
-    class User < Sequel::Model; end
+    class User < Sequel::Model
+      def graph
+        graph = self.class.where(twitter_id: self.friend_ids.to_a - [self.twitter_id])
+                          .select_hash(:twitter_id, :friend_ids)
+        Hash[graph.map {|id,friend_ids| [id, friend_ids - [self.twitter_id]] }]
+      end
+    end
+
     class List < Sequel::Model; end
   end
 end
