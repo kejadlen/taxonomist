4067686cf71f9b67ac51e84f65cc10dc15036591
Alpha Chen <alpha.chen@gmail.com>
2015-12-07 07:55:47 -0800

    Move auth routes into its own file

--- lib/taxonomist/web/app.rb
+++ lib/taxonomist/web/app.rb
@@ -12,52 +12,10 @@ module Taxonomist
 
     opts[:root] = File.expand_path("..", __FILE__)
 
-    plugin :multi_route
     plugin :render, views: "views"
 
-    route("auth") do |r|
-      r.get "sign_in" do
-        twitter_oauth = Twitter::OAuth.new(api_key: ENV["TWITTER_API_KEY"],
-                                           api_secret: ENV["TWITTER_API_SECRET"])
-
-        callback = "http://#{r.host_with_port}/auth/callback"
-        request_token = twitter_oauth.request_token(callback: callback)
-
-        r.session[:token] = request_token["oauth_token"]
-        r.session[:token_secret] = request_token["oauth_token_secret"]
-
-        url = "https://api.twitter.com/oauth/authorize?oauth_token=#{r.session[:token]}"
-        r.redirect url
-      end
-
-      # TODO Verify oauth_token is the same as before?
-      r.get "callback" do
-        token = r.session.delete(:token)
-        token_secret = r.session.delete(:token_secret)
-
-        twitter_oauth = Twitter::OAuth.new(api_key: ENV["TWITTER_API_KEY"],
-                                           api_secret: ENV["TWITTER_API_SECRET"])
-        access_token = twitter_oauth.access_token(token: token, token_secret: token_secret,
-                                                  oauth_verifier: r.params["oauth_verifier"])
-
-        oauth_token = access_token["oauth_token"]
-        oauth_token_secret = access_token["oauth_token_secret"]
-        user_id = access_token["user_id"]
-        screen_name = access_token["screen_name"]
-
-        user = Models::User.find_or_create(twitter_id: user_id)
-        user.update(access_token: oauth_token, access_token_secret: oauth_token_secret)
-
-        r.session[:user_id] = user_id
-
-        r.redirect "/"
-      end
-
-      r.get "sign_out" do
-        r.session.delete(:user_id)
-        r.redirect "/"
-      end
-    end
+    plugin :multi_route
+    require_relative "routes/auth"
 
     route do |r|
       r.multi_route
