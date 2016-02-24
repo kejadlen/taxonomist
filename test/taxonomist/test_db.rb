4c8001c8dfc1e857167e62ad4c888e480577f401
Alpha Chen <alpha.chen@gmail.com>
2016-02-24 07:27:38 -0800

    Temporarily remove User#graph

--- test/taxonomist/test_db.rb
+++ test/taxonomist/test_db.rb
@@ -3,18 +3,18 @@ require_relative "../karate_club"
 
 module Taxonomist
   class TestUser < Test
-    def test_friend_graph
-      KARATE_CLUB.each do |id, friend_ids|
-        Models::User.create(twitter_id: id, friend_ids: friend_ids)
-      end
+    # def test_friend_graph
+    #   KARATE_CLUB.each do |id, friend_ids|
+    #     Models::User.create(twitter_id: id, friend_ids: friend_ids)
+    #   end
 
-      user = Models::User[twitter_id: 1]
-      graph = user.graph
+    #   user = Models::User[twitter_id: 1]
+    #   graph = user.graph
 
-      refute_includes graph.keys, user.twitter_id
-      graph.each do |_, friends|
-        refute_includes friends, user.twitter_id
-      end
-    end
+    #   refute_includes graph.keys, user.twitter_id
+    #   graph.each do |_, friends|
+    #     refute_includes friends, user.twitter_id
+    #   end
+    # end
   end
 end
