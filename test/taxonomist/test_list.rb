a1765d385f6d2a889316077c1ba55b58dbcf974e
Alpha Chen <alpha.chen@gmail.com>
2016-03-22 07:34:23 -0700

    Make TwitterStub easier to use for instances

--- test/taxonomist/test_list.rb
+++ test/taxonomist/test_list.rb
@@ -7,9 +7,8 @@ module Taxonomist
   class TestList < Test
     def test_list
       remote_ids = [2, 4, 6, 8]
-      TwitterStub.stubs[:lists_members] = remote_ids
 
-      list = List.new(12345, TwitterStub.new)
+      list = List.new(12345, TwitterStub.new(lists_members: remote_ids))
       list.pull!
 
       assert_equal remote_ids, list.remote_ids
--- test/twitter_stub.rb
+++ test/twitter_stub.rb
@@ -6,12 +6,14 @@ module Taxonomist
       attr_accessor :stubs
     end
 
-    def initialize(*)
-      self.class.stubs ||= {}
+    attr_reader :stubs
+
+    def initialize(stubs={})
+      @stubs = (self.class.stubs || {}).merge(stubs)
     end
 
     def method_missing(name, *args)
-      answer = self.class.stubs[name]
+      answer = self.stubs[name]
       case answer
       when Proc
         answer.call(*args)
@@ -21,7 +23,7 @@ module Taxonomist
     end
 
     def respond_to_missing?(name, include_private=false)
-      self.class.stubs.has_key?(name)
+      self.stubs.has_key?(name)
     end
   end
 end
