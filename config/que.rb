068f2cca6ad1442d4aecdc67822546d0a3bc2bb1
Alpha Chen <alpha.chen@gmail.com>
2016-03-28 15:59:25 -0700

    Fix quote marks

--- config/que.rb
+++ config/que.rb
@@ -1,7 +1,7 @@
-$LOAD_PATH.unshift(File.expand_path("../../lib", __FILE__))
-require "taxonomist"
+$LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
+require 'taxonomist'
 
-require "que"
+require 'que'
 Que.connection = Taxonomist::DB
 
-$stdout.sync = true if Gem::Version.new(Que::Version) <= Gem::Version.new("0.11.2")
+$stdout.sync = true if Gem::Version.new(Que::Version) <= Gem::Version.new('0.11.2')
--- lib/taxonomist.rb
+++ lib/taxonomist.rb
@@ -1,4 +1,4 @@
-require_relative "taxonomist/db"
-require_relative "taxonomist/jobs"
-require_relative "taxonomist/slpa"
-require_relative "taxonomist/twitter"
+require_relative 'taxonomist/db'
+require_relative 'taxonomist/jobs'
+require_relative 'taxonomist/slpa'
+require_relative 'taxonomist/twitter'
